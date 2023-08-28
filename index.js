36d522086d6e5063a98dfcb919770ffb30d56f12
rahulsanjeev789 <139852236+rahulsanjeev789@users.noreply.github.com>
2023-08-28 13:38:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 let count=0
 let countEl=document.getElementById("count-el")
+let saveEl=document.getElementById("save-el")
 function increment(){
     count++
     countEl.innerText=count
 }
 
 function save(){
-    console.log(count)
+
+    let countStr= count + "-" 
+    saveEl.textContent+= countStr
+    countEl.innerText=0
+    count=0
+
+
+
+
 }
 
